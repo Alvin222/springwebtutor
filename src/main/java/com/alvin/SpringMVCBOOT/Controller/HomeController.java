f60e45dcce39a74d2afd644981d8b044b135d713
alvin222 <alvinroidoma72@gmail.com>
2021-08-05 19:10:14 +0700

    update

--- src/main/java/com/alvin/SpringMVCBOOT/Controller/HomeController.java
+++ src/main/java/com/alvin/SpringMVCBOOT/Controller/HomeController.java
@@ -1,16 +1,17 @@
 package com.alvin.SpringMVCBOOT.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
-    public String home(){
-        return "index.html";
+    @RequestMapping("add")
+    public String add(@RequestParam() int num1 , @RequestParam() int num2){
+        return "result";
     }
+
+
 
 }
