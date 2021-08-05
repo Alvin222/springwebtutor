7f7742ccae1ed5886642bc1c54317546768565ca
alvin222 <alvinroidoma72@gmail.com>
2021-08-05 20:02:07 +0700

    update

--- src/main/java/com/alvin/SpringMVCBOOT/Controller/HomeController.java
+++ src/main/java/com/alvin/SpringMVCBOOT/Controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.alvin.SpringMVCBOOT.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping("add")
+    @RequestMapping("/add")
     public String add(@RequestParam() int num1 , @RequestParam() int num2){
         return "result";
     }
