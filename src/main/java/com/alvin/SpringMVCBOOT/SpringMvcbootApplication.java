db30709387aefb48abca4b442872b7ea959c1d65
alvin222 <alvinroidoma72@gmail.com>
2021-08-05 10:59:10 +0700

    first commit

---
+++ src/main/java/com/alvin/SpringMVCBOOT/SpringMvcbootApplication.java
@@ -0,0 +1,13 @@
+package com.alvin.SpringMVCBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcbootApplication.class, args);
+	}
+
+}
